aa9da233a0633c1d383cb6cf564bf09c81e8bb3d
chan <kdch9495@gmail.com>
2022-06-05 22:51:48 +0900

    [INI] Create Customer.js

--- React Project Tutorial/management/src/App.js
+++ React Project Tutorial/management/src/App.js
@@ -1,14 +1,69 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import Customer from './components/Customer'
 import './App.css';
 
-// 화면에 대한 내용의 출력을 담당하는 부분
-function App() {
-  return (
-    <div className='gray-background'>
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
-  );
+const customers = [
+  {
+    'id' : 1,
+    'image' : 'http://placeimg.com/64/64/1',
+    'name' : '동찬',
+    'birthday' : '951221',
+    'gender' : '남성',
+    'job' : '직짱인'
+  },
+  {
+    'id' : 2,
+    'image' : 'http://placeimg.com/64/64/2',
+    'name' : '길동',
+    'birthday' : '951222',
+    'gender' : '남성',
+    'job' : '프로그래머'
+  },
+  {
+    'id' : 3,
+    'image' : 'http://placeimg.com/64/64/3',
+    'name' : '찬동',
+    'birthday' : '951223',
+    'gender' : '남성',
+    'job' : '직짱인3'
+    }
+]
+
+class App extends Component {
+  render() { 
+    return (
+      <div>
+        {
+        /*
+        // 하나만 화면에 출력하고 싶을 떄는 아래와 같이 사용 
+        <Customer
+          id={customers[0].id}
+          image={customers[0].iamge}
+          name={customers[0].name}
+          birthday={customers[0].birthday}
+          gender={customers[0].gender}
+          job={customers[0].job}
+        />
+        */
+        }
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id} // map을 사용할때는 key를 반드시 사용해야 함 
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            ) 
+          })
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
